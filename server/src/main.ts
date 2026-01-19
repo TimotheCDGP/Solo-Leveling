6532ca03343451378663f0541daaf4e2ec139f6e
prince-myssie <prince.myssie@vocads.com>
2026-01-19 14:27:51 +0100

    refactor: remove App module and related files; integrate Prisma module as the main application entry point

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
+import { PrismaModule } from './prisma/prisma.module';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(PrismaModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Error starting the application:', error);
+  process.exit(1);
+});
